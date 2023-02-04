3126ad9a4c4a49f7909849848dc3e184a5c381e5
CaoKhaHieu <caokhahieu9@gmail.com>
2023-02-04 21:58:22 +0700

    fix import

--- routes/upload.route.js
+++ routes/upload.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { uploadController } from "../controllers/upload.controller";
+import { uploadController } from "../controllers/upload.controller.js";
 import { upload } from "../helper/index.js";
 import { isAuth } from "../middleware/auth.middleware.js";
 
