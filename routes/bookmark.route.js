4f88e2bc797a22e8a87d383349e1532515aeee67
CaoKhaHieu <caokhahieu9@gmail.com>
2023-02-04 21:52:26 +0700

    fix import

--- routes/bookmark.route.js
+++ routes/bookmark.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { bookmarkController } from "../controllers/bookmark.controller.js";
-import { isAuth } from "../middleware/auth.middleware";
+import { isAuth } from "../middleware/auth.middleware.js";
 
 const bookmarkRouter = express.Router();
 
