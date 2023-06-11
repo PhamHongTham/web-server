acc5c2fa4c3166283a413d7322f73535fa05e0db
PhamHongTham <ptht101002@gmail.com>
2023-06-11 13:31:20 +0700

    tét

--- helper/passport.js
+++ helper/passport.js
@@ -1,25 +1,25 @@
-import passport from "passport";
-import * as googleAuth from 'passport-google-oauth20';
+// import passport from "passport";
+// import * as googleAuth from 'passport-google-oauth20';
 
-const GoogleStrategy = googleAuth.Strategy;
+// const GoogleStrategy = googleAuth.Strategy;
 
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/api/auth/google/callback",
-    },
-    async function (accessToken, refreshToken, profile, done) {
-      done(null, profile)
-    }
-  )
-);
+// passport.use(
+//   new GoogleStrategy(
+//     {
+//       clientID: process.env.GOOGLE_CLIENT_ID,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//       callbackURL: "/api/auth/google/callback",
+//     },
+//     async function (accessToken, refreshToken, profile, done) {
+//       done(null, profile)
+//     }
+//   )
+// );
 
-passport.serializeUser((user, done) => {
-  done(null, user);
-});
+// passport.serializeUser((user, done) => {
+//   done(null, user);
+// });
 
-passport.deserializeUser((user, done) => {
-  done(null, user);
-});
+// passport.deserializeUser((user, done) => {
+//   done(null, user);
+// });
